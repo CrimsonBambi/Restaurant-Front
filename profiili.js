b89cdcb5ab711cbd92aacc78151e78e2f7e0fe85
CrimsonBambi <gerli.hirv@metropolia.fi>
2025-04-06 14:15:30 +0300

    modal for registeration

--- profiili.js
+++ profiili.js
@@ -1,16 +1,31 @@
-// References
-const openModalButton = document.getElementById('open-modal');
-const closeModalButton = document.getElementById('close-modal');
+// login references
+const openLogin = document.getElementById('open-login-modal');
+const closeLogin = document.getElementById('close-login');
 const loginModal = document.getElementById('login-modal');
 
-// Open the modal
-openModalButton.addEventListener('click', (event) => {
+// register references
+const openRegister = document.getElementById('open-register-modal');
+const closeRegister = document.getElementById('close-register');
+const registerModal = document.getElementById('register-modal');
+
+// Open the login modal
+openLogin.addEventListener('click', (event) => {
     event.preventDefault(); // Prevent default link behavior
     loginModal.showModal(); 
 });
 
-// Close the modal 
-closeModalButton.addEventListener('click', () => {
+// Close the login modal 
+closeLogin.addEventListener('click', () => {
     loginModal.close(); 
 });
 
+// Open the register modal
+openRegister.addEventListener('click', (event) => {
+    event.preventDefault(); // Prevent default link behavior
+    registerModal.showModal(); 
+});
+
+// Close the login modal 
+closeRegister.addEventListener('click', () => {
+    registerModal.close(); 
+});
