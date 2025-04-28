fa38910a20adca02328770aaaf2303d8bee99a99
TaruNipuli <taru.nipuli@metropolia.fi>
2025-04-28 10:32:46 +0300

    Add react structure to the project

--- menu/menu.js
+++ menu/menu.js
@@ -13,31 +13,31 @@ const closeButtons = document.querySelectorAll(".close");
 
 // Open modals when images are clicked
 menu1Img.addEventListener("click", () => {
-    menu1Modal.style.display = "block";
+  menu1Modal.style.display = "block";
 });
 
 menu2Img.addEventListener("click", () => {
-    menu2Modal.style.display = "block";
+  menu2Modal.style.display = "block";
 });
 
 menu3Img.addEventListener("click", () => {
-    menu3Modal.style.display = "block";
+  menu3Modal.style.display = "block";
 });
 
 // Close modals when close buttons are clicked
-closeButtons.forEach(button => {
-    button.addEventListener("click", () => {
-        button.parentElement.parentElement.style.display = "none";
-    });
+closeButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    button.parentElement.parentElement.style.display = "none";
+  });
 });
 
 // Close modals when clicking outside the modal content
 window.addEventListener("click", (event) => {
-    if (event.target === menu1Modal) {
-        menu1Modal.style.display = "none";
-    } else if (event.target === menu2Modal) {
-        menu2Modal.style.display = "none";
-    } else if (event.target === menu3Modal) {
-        menu3Modal.style.display = "none";
-    }
+  if (event.target === menu1Modal) {
+    menu1Modal.style.display = "none";
+  } else if (event.target === menu2Modal) {
+    menu2Modal.style.display = "none";
+  } else if (event.target === menu3Modal) {
+    menu3Modal.style.display = "none";
+  }
 });
